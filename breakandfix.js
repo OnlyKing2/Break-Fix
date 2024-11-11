5bab6c227461a6a21f1bf263b6718d7819d7644c
OnlyKing2 <130361822+OnlyKing2@users.noreply.github.com>
2024-11-11 10:03:02 +0000

    Create breakandfix.js

---
+++ breakandfix.js
@@ -0,0 +1,42 @@
+// This class is due to return information to car owners
+// it is based on the fuel they input
+// this should return a message to the user based on the method that is called
+
+class Fuel {
+    constructor(fuelName, price, fuelType) {
+        this.name = fuelName;
+        this.origin = price;
+        this.fueltype = fuelType;
+    }
+
+
+    getfuelPrice() {
+        this.origin
+
+      
+        // here needs to be some code to tell the price of this fuel
+    }
+
+    getFuelDetails {
+        this.name = fuelName;
+        this.origin = price;
+        this.fueltype = fuelType;
+        
+        // here needs to be some code to tell the all the details of this fuel
+
+    }
+    
+   getRecommendation {}
+   
+
+
+   // here needs to be some code that returns a recommendation on the car owner going forward
+   
+
+}
+
+var actor = new Tractor("Liam", Navan, "Action");
+console.log(actor.getGenre()) 
+console.log(actor2.getActorDetails)
+
+// you must comment the code correctly 
